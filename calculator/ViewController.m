8ca23ddb2b04bd6b8c3f4f36f0196389c6e04e37
SudoKel <kjoanes@gmail.com>
2017-01-29 21:50:02 -0330

    added functionality for basic binary operators

--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -11,11 +11,14 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *txtDisplay;
 @property (nonatomic, assign) BOOL decimalPoint;
+@property (nonatomic, assign) BOOL clearScreen;
 @property (nonatomic, assign) NSString *operator;
 @property (nonatomic, assign) float firstOperand;
 @property (nonatomic, assign) float secondOperand;
 
 - (IBAction)operandBtn:(id)sender;
+- (IBAction)binaryOperator:(id)sender;
+- (IBAction)evaluateExpression:(id)sender;
 
 @end
 
@@ -26,6 +29,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self setDecimalPoint:NO];
+    [self setOperator:@""];
 }
 
 - (void)didReceiveMemoryWarning
@@ -69,19 +73,87 @@
     // Otherwise append the label text to display
     else
     {
+        // Clear field for next operand
+        if ([self clearScreen])
+        {
+            currentText = @"";
+            [self setClearScreen:NO];
+        }
+        
         // Avoid more than one decimal point
         if (![self decimalPoint] && ![currentText isEqualToString:@""])
         {
             _txtDisplay.text = [currentText stringByAppendingString:btnText];
             
+            // Set flag if decimal point has been added
             if ([btnText isEqualToString:@"."])
                 [self setDecimalPoint:YES];
         }
         else
         {
+            // If current button is not a decimal point then append its text to display
             if (![btnText isEqualToString:@"."])
                 _txtDisplay.text = [currentText stringByAppendingString:btnText];
         }
     }
 }
+
+- (IBAction)binaryOperator:(id)sender
+{
+    // Set current text in display to be the first operand
+    NSString *currentText = _txtDisplay.text;
+    [self setFirstOperand:[currentText floatValue]];
+    
+    // Get label text of button
+    NSString *btnText = [(UIButton *)sender currentTitle];
+    
+    // Set the current operator
+    [self setOperator:btnText];
+    
+    // Set flags
+    [self setDecimalPoint:NO];
+    [self setClearScreen:YES];
+}
+
+- (IBAction)evaluateExpression:(id)sender
+{
+    // Set current text in display to be the second operand
+    NSString *currentText = _txtDisplay.text;
+    [self setSecondOperand:[currentText floatValue]];
+    
+    // Evaluate expression based on operands and operator
+    if ([[self operator] isEqualToString:@"+"])
+    {
+        float sum = [self firstOperand] + [self secondOperand];
+        _txtDisplay.text = [NSString stringWithFormat:@"%.3f", sum];
+        // Set result to be first operand
+        [self setFirstOperand:sum];
+    }
+    else if ([[self operator] isEqualToString:@"-"])
+    {
+        float difference = [self firstOperand] - [self secondOperand];
+        _txtDisplay.text = [NSString stringWithFormat:@"%.3f", difference];
+        [self setFirstOperand:difference];
+    }
+    else if ([[self operator] isEqualToString:@"÷"])
+    {
+        float quotient = [self firstOperand] / [self secondOperand];
+        _txtDisplay.text = [NSString stringWithFormat:@"%.3f", quotient];
+        [self setFirstOperand:quotient];
+    }
+    else
+    {
+        float product = [self firstOperand] * [self secondOperand];
+        _txtDisplay.text = [NSString stringWithFormat:@"%.3f", product];
+        [self setFirstOperand:product];
+    }
+    
+    // Reset second operand to '0'
+    [self setSecondOperand:0];
+    // Set flag
+    [self setDecimalPoint:YES];
+    
+}
+
+
 @end
